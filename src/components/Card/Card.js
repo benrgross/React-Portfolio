3ff6c7364967c1b76bddcc6851214c130566d6fb
Ben Gross <benrgross@gmail.com>
2021-03-18 17:21:18 -0400

    animatiuon working

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,11 +1,12 @@
 import React from "react";
+import Image from "../Image/Image";
 import { useBoolean } from "react-hanger";
 import "./style.css";
 
 function Card({ name, web, repo, image, tech }) {
   const showInfo = useBoolean(false);
 
-  const handleZoom = () => {};
+  //   const handleZoom = () => {};
 
   return (
     <div className="col-md-4 col zoom">
@@ -13,12 +14,13 @@ function Card({ name, web, repo, image, tech }) {
         className="card container-fluid img-container "
         style={{ width: "20rem", padding: "0px" }}
       >
-        <img
+        {/* <img
           src={image}
           className="card-img-top img"
           alt={name}
           onClick={showInfo.toggle}
-        />
+        /> */}
+        <Image source={image} />
         {showInfo.value ? (
           <div style={{ padding: "5px" }} className="card-body">
             <p className="card-text">
